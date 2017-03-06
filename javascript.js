9ba3788cc73d18e3313b3473409fefc838b97006
Peter J. Truong <hp5340@gmail.com>
2017-03-06 08:50:23 -0800

    fix mobile jumbotron

--- javascript.js
+++ javascript.js
@@ -3,21 +3,7 @@ var a = $(".navbar").offset().top;
 $(document).ready(function(){
 
   $('.navbar').css({"padding-top":"3%"});
-       $('.navbar').css({"padding-bottom":"3%"});
-       $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
-    }
-  });
+  $('.navbar').css({"padding-bottom":"3%"});  
 
 });
 
@@ -28,7 +14,7 @@ $(document).scroll(function(){
       
        $('.navbar').css({"padding-top":""});
        $('.navbar').css({"padding-bottom":""});
-       $('.navbar').css({"border-bottom":""});
+     
         
        
     } else {
